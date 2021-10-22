89bb01d028e8c74ea87299695496b2afb9ab07a8
searayeah <insslava@gmail.com>
2021-10-22 11:16:45 +0300

    ver2

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,6 @@
 import os
-import pandas as pd
-import numpy as np
 import gspread
-from random import shuffle
+from random import shuffle, choice
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
@@ -10,7 +8,15 @@ from telegram.ext import (
     CommandHandler,
     CallbackQueryHandler,
     ConversationHandler,
+    PicklePersistence,
 )
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+log = logging.getLogger(__name__)
 
 SHEET_KEY = os.environ["sheet_key"]
 SHEET_NAME = "sheet1"
@@ -28,104 +34,125 @@ TOKEN_NAMES_LIST = [
 ]
 TOKEN = {item: os.environ[item].replace("\\n", "\n") for item in TOKEN_NAMES_LIST}
 BOT_TOKEN = os.environ["bot_token"]
-
-
-QUESTION, ANSWER = range(2)
+QUESTION = range(1)
 
 
 def get_questions(sheet_key, sheet_name, token_names_list, token):
     gc = gspread.service_account_from_dict(token)
     sh = gc.open_by_key(sheet_key)
     worksheet = getattr(sh, sheet_name)
-    return pd.DataFrame(worksheet.get_all_records())
+    log.info("Loaded questions from google sheets")
+    return {key: value for (key, value) in enumerate(worksheet.get_all_values())}
+
+
+def set_keyboard(x, y, z):
+    return [
+        [
+            InlineKeyboardButton(x, callback_data=x),
+            InlineKeyboardButton(y, callback_data=y),
+        ],
+        [
+            InlineKeyboardButton(z, callback_data=z),
+        ],
+    ]
 
 
 def start(update, context):
-    update.message.reply_text("Чтобы закончить пропишите /stop")
-    global questions
-    questions = get_questions(SHEET_KEY, SHEET_NAME, TOKEN_NAMES_LIST, TOKEN)
+    if "questions" not in context.user_data:
+        context.user_data["questions"] = get_questions(
+            SHEET_KEY, SHEET_NAME, TOKEN_NAMES_LIST, TOKEN
+        )
+        context.user_data["questions"].pop(0)
+    update.message.reply_text(
+        f"Чтобы ресетнуть список вопросов, пропишите /reset. "
+        + f'У вас осталось {len(context.user_data["questions"])} нерешённых вопросов.'
+        + " Список сам ресетится каждые 24 часа, "
+        + "поэтому ваши решенные вопросы больше этого времени не сохранятся"
+        + " (бесплатный хостинг вайпит серв каждые 24 часа)"
+    )
+    log.info("Start called")
     form_question(update, context)
     return QUESTION
 
 
 def form_question(update, context):
-    random_number = np.random.randint(0, questions.shape[0])
-    global data
-    data = list(filter(None, questions.iloc[random_number].values))
-
-    global question
-    global answer
-    question = data.pop(0)
-    answer = data[0]
+    log.info(
+        f'Form_question called with {len(context.user_data["questions"])} questions'
+    )
+    log.info(context.user_data["questions"])
+    if len(context.user_data["questions"]) == 0:
+        update.message.reply_text("*Вопросы кончились*", parse_mode="markdown")
+        return reset(update, context)
+    context.user_data["question_id"] = choice(
+        list(context.user_data["questions"].keys())
+    )
+    log.info(f'Random question with number {context.user_data["question_id"]}')
+    data = list(
+        map(
+            str,
+            context.user_data["questions"][context.user_data["question_id"]],
+        )
+    )
+    log.info(f"question data {data}")
+    context.user_data["question"] = data.pop(0)
+    context.user_data["answer"] = data[0]
     keyboard = []
-    print(data)
+    shuffle(data)
     if any([True for x in data if len(x) >= 60]):
-        print("ENTERED")
-        shuffle(data)
-        answer = str(data.index(answer) + 1)
-        question = f"{question} \n\n1.{data[0]}\n\n2.{data[1]}\n\n3.{data[2]}"
-        keyboard = [
-            [
-                InlineKeyboardButton("1", callback_data="1"),
-                InlineKeyboardButton("2", callback_data="2"),
-            ],
-            [
-                InlineKeyboardButton("3", callback_data="3"),
-            ],
-        ]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        update.message.reply_text(question, reply_markup=reply_markup)
+        context.user_data["answer"] = str(data.index(context.user_data["answer"]) + 1)
+        context.user_data[
+            "question"
+        ] = f'{context.user_data["question"]}\n\n1.{data[0]}\n\n2.{data[1]}\n\n3.{data[2]}'
+        keyboard = set_keyboard("1", "2", "3")
     else:
-        if len(data) == 3:
-            shuffle(data)
-            keyboard = [
-                [
-                    InlineKeyboardButton(str(data[0]), callback_data=str(data[0])),
-                    InlineKeyboardButton(str(data[1]), callback_data=str(data[1])),
-                ],
-                [
-                    InlineKeyboardButton(str(data[2]), callback_data=str(data[2])),
-                ],
-            ]
+        keyboard = set_keyboard(data[0], data[1], data[2])
 
-        reply_markup = InlineKeyboardMarkup(keyboard)
-
-        update.message.reply_text(question, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(context.user_data["question"], reply_markup=reply_markup)
 
 
 def button(update, context):
     query = update.callback_query
     query.answer()
-    if query.data == answer:
+    if query.data == context.user_data["answer"]:
         query.edit_message_text(
-            text=question + "\n" + f"Верно - *{query.data}*",
+            text=context.user_data["question"] + "\n\n" + f"Верно - *{query.data}*",
             parse_mode="markdown",
+        )
+
+        context.user_data["questions"].pop(context.user_data["question_id"])
+        log.info(
+            f"Right answer, dropping question from pool. "
+            + f'Now pool size {len(context.user_data["questions"])}'
         )
     else:
         query.edit_message_text(
-            text=question + "\n\n" + f"Неверно!!! Правильный ответ - *{answer}*",
+            text=context.user_data["question"]
+            + "\n\n"
+            + f'Неверно!!! Правильный ответ - *{context.user_data["answer"]}*',
             parse_mode="markdown",
         )
+        log.info("Wrong answer")
     form_question(query, context)
 
 
-def stop(update, context):
+def reset(update, context):
+    context.user_data.clear()
     update.message.reply_text("Пропишите /start, чтобы начать по новой")
     return ConversationHandler.END
 
 
-def main(bot_token) -> None:
-
-    updater = Updater(bot_token)
+def main(bot_token):
+    persistence = PicklePersistence(filename="bot/base", store_callback_data=True)
+    updater = Updater(bot_token, persistence=persistence, use_context=True)
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         allow_reentry=True,
-        states={
-            QUESTION: [
-                CallbackQueryHandler(button),
-            ],
-        },
-        fallbacks=[CommandHandler("stop", stop)],
+        states={QUESTION: [CallbackQueryHandler(button)]},
+        fallbacks=[CommandHandler("reset", reset)],
+        conversation_timeout=360,
+        persistent=True,
+        name="conv_handler_save",
     )
     updater.dispatcher.add_handler(conv_handler)
 
