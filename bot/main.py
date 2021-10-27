bff1b31250a98bc12349592271ecdc2cf4edb1d9
searayeah <insslava@gmail.com>
2021-10-27 14:06:16 +0300

    img fix

--- bot/main.py
+++ bot/main.py
@@ -54,7 +54,8 @@ def set_keyboard(x, y, z):
 def get_questions(sheet_key, sheet_name, token_names_list, token, user):
     gc = gspread.service_account_from_dict(token)
     sh = gc.open_by_key(sheet_key)
-    worksheet = getattr(sh, sheet_name)
+    # worksheet = getattr(sh, sheet_name)
+    worksheet = sh.get_worksheet(1)
     log.info(f"User {user} loaded questions from Google")
     return {key: value for (key, value) in enumerate(worksheet.get_all_values())}
 
@@ -122,11 +123,14 @@ def form_question(update, context):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     if context.user_data["quest_photo"] != "":
-        update.message.reply_photo(
-            photo=context.user_data["quest_photo"],
-            caption=context.user_data["quest"],
-            reply_markup=reply_markup,
-        )
+        with open(
+            "bot/images/" + context.user_data["quest_photo"] + ".png", "rb"
+        ) as image:
+            update.message.reply_photo(
+                photo=image,
+                caption=context.user_data["quest"],
+                reply_markup=reply_markup,
+            )
     else:
         update.message.reply_text(context.user_data["quest"], reply_markup=reply_markup)
 
